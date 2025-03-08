d8fb6598c3a0fbe8fa254abc9e37eddac9c6c2c7
Mohamad ALRamahi <mohamadramahi0@gmail.com>
2025-03-08 18:14:13 +0300

    create login cubit
    create login cubit

---
+++ lib/cubit/login_cubit.dart
@@ -0,0 +1,39 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+// States
+abstract class LoginState extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class LoginInitial extends LoginState {}
+class LoginLoading extends LoginState {}
+class LoginSuccess extends LoginState {}
+class LoginFailure extends LoginState {
+  final String error;
+  LoginFailure(this.error);
+
+  @override
+  List<Object> get props => [error];
+}
+
+// Cubit Class
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(LoginInitial());
+
+  void login(String email, String password) {
+    if (email.isEmpty || password.isEmpty) {
+      emit(LoginFailure("Fields cannot be empty"));
+      return;
+    }
+
+    emit(LoginLoading());
+
+    // Simulate a delay for login attempt
+    Future.delayed(const Duration(seconds: 2), () {
+      // Example of checking mock credentials
+
+    });
+  }
+}
